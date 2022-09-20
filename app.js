c7e584f6f2da06c97852a8bdfebe52fa2e67858e
Gabriel Salandra <ga.salandra@gmail.com>
2022-09-20 13:26:48 +0000

    saveback

--- app.js
+++ app.js
@@ -6,7 +6,27 @@ const methodOverride = require('method-override');
 const cookieParser = require("cookie-parser");
 const sessions = require('express-session');
 const flash = require('connect-flash');
+const session = require("express-session");
+const MongoStore = require('connect-mongo');
 
+// // session
+const store = MongoStore.create({
+	mongoUrl: "mongodb+srv://gadmin:gz695VWqzWeSnSxr@cluster0.d74y6.mongodb.net/evaluaciones?retryWrites=true&w=majority",
+	secret: 'secretkey',
+	touchAfter: 24*60*60
+})
+const sessionConfig = {
+	store,
+	name: "session",
+	secret: 'secretkey',
+	resave: false,
+	saveUninitialized: false,
+	cookie: {
+		httpOnly: true,
+		expires: Date.now() + 1000 * 60 *60 * 24 * 7,
+		maxAge: 1000 * 60 *60 * 24 * 7
+	}
+}
 //Especificaciones
 app.use(express.static('public'))
 app.set('view engine','ejs');
@@ -15,7 +35,7 @@ app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(sessions({secret: 'secretkey', resave: false, saveUninitialized: false}));
+app.use(sessions(sessionConfig));
 app.use(flash());
 
 app.use((req,res,next)=>{
